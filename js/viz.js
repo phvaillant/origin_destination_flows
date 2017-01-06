eea3330a497a33924255a8f690f03aca56b590e9
debricassart <philippe.vaillant90@gmail.com>
2017-01-05 17:15:44 -0800

    added filter, graph, etc.

--- js/viz.js
+++ js/viz.js
@@ -1,3 +1,8 @@
+//declare the filters variable
+var mode = Array.apply(0, Array(5)).map(function (x, y) { return y + 1; });
+var purpose = Array.apply(0, Array(4)).map(function (x, y) { return y + 1; });
+var hour = Array.apply(0, Array(24)).map(function (x, y) { return y; });
+
 $(document).ready(function(){
 
   //declare map
@@ -48,22 +53,128 @@ $(document).ready(function(){
     var tractById = d3.map();
 
     var tracts = mapFeatures.selectAll('g')
-                .data(collection.features)
-                .enter().append('g')
-                  .attr('class',"tract")
-                  .each(function(d) {
-                    tractById.set(d.properties.GEOID10, d);
-                    d.incoming = [];
-                    d.outgoing = [];
-                    d.total_trips = 0;
-                    var position = path.centroid(d);
-                    d.x = position[0];
-                    d.y = position[1];
-                 }); //end of each tracts function
+                  .data(collection.features)
+                  .enter().append('g')
+                    .attr('class',"tract")
+                    //add information for each tract: geoid, incoming and outgoing trips (list), total trips, and position of centroid
+                    .each(function(d) {
+                      tractById.set(d.properties.GEOID10, d);
+                      d.incoming = [];
+                      d.outgoing = [];
+                      d.total_trips = 0;
+                      var position = path.centroid(d);
+                      d.x = position[0];
+                      d.y = position[1];
+                   }); //end of each tracts function
 
     tracts.append("path")
               .attr("class","tract_cell")
-              .attr("d",path);
+              .attr("d",path)
+
+    //filter by: exclude intra-tract flows, mode, purpose, time start
+    trips = trips_seattle_modified.filter(function(d) {return ((d.o_tract != d.d_tract) & (mode.indexOf(+d.mode) != -1) & (purpose.indexOf(+d.d_purpose)!= -1) & (hour.indexOf(+d.time_start_hhmm) != -1))});
+
+    //grouping by origin and destination tract and summing number of trips
+    var trips_count = d3.nest()
+            .key(function(d) { return d.o_tract; })
+            .key(function(d) { return d.d_tract; })
+            .rollup(function(trips) { return d3.sum(trips, function(d) {return d.count}) })
+            .entries(trips);
+
+    var max_weight = 0;
+
+    //processing data: getting links, max weight for one link, and the total of trips
+    trips_count.forEach(function(d) {
+                    var source = tractById.get(d.key);
+                    d.values.forEach(function(d) {
+                      max_weight = Math.max(max_weight, d.value);
+                      var target = tractById.get(d.key);
+                      link = {source: source, target: target, weight: d.value};
+                      //in d3 v4 value instead of values
+                      source.total_trips += d.value;
+                      source.outgoing.push(link);
+                      target.incoming.push(link);
+                    });
+                  });
+
+    //specifying line scale
+    var lineScale = d3.scaleLinear()
+          .domain([0, max_weight])
+          .range([0.2, 20]);
+
+    //drawing lines for flows
+    tracts.append("g")
+          .attr("class","tract_links")
+          .selectAll("line")
+          .data(function(d) {return d.outgoing})
+              .enter().append("line")
+              .attr("class","tract_link")
+              .attr("x1", function(d) { return d.source.x})
+              .attr("x2", function(d) { return d.target.x})
+              .attr("y1", function(d) { return d.source.y})
+              .attr("y2", function(d) { return d.target.y})
+              .attr("stroke","gray")
+              .attr("stroke-width", function(d) {return lineScale(d.weight)});
+
+    var tooltip_id_1 = d3.select('#tooltip_1_id');
+    var tooltip_details_1 = d3.select('#tooltip_1_detail');
+    var tooltip_id_2 = d3.select('#tooltip_2_id');
+    var tooltip_details_2 = d3.select('#tooltip_2_detail');
+
+    //variable to store tract clicked
+    var tract_clicked = 0;
+    var second_tooltip = false;
+
+    //add info on mouseover
+    d3.selectAll('path')
+      .on('click', function(d) {
+        tracts.selectAll(".tract_clicked").classed("tract_clicked", false);
+        tracts.selectAll(".links_clicked").attr("class","tract_links");
+        if(tract_clicked != d.properties.GEOID10) {
+          d3.select(this).classed("tract_clicked", true);
+          d3.select(this.parentNode).selectAll("g").attr("class","links_clicked");
+          tracts.selectAll(".tract_links").classed("hidden", true);
+          tract_clicked = d.properties.GEOID10;
+          tooltip_id_1.classed('hidden', false)
+                        .html("GEOID: " + d.properties.GEOID10);
+          tooltip_details_1.classed('hidden', false)
+                        .html("Total outgoing trips: " + d.total_trips);
+          second_tooltip = true;
+        }
+        else {
+          tracts.selectAll(".tract_links").classed("hidden", false);
+          tract_clicked = 0;
+          second_tooltip = false;
+          tooltip_id_2.classed('hidden', true);
+          tooltip_details_2.classed('hidden', true);
+        }
+      })
+      .on("mouseover", function(d) {
+                if (second_tooltip) {
+                  tooltip_id_2.classed('hidden', false)
+                        .html("GEOID: " + d.properties.GEOID10);
+                  var outgoing_trips = trips_count.filter(function(a) {return a.key==tract_clicked})[0].values.filter(function(a) {return a.key==d.properties.GEOID10})[0];
+                  outgoing_trips ? outgoing_trips = outgoing_trips.value : outgoing_trips = 0;
+                  tooltip_details_2.classed('hidden', false)
+                        .html("Trips to this tract: " + outgoing_trips);
+                }
+                else {
+                  tooltip_id_1.classed('hidden', false)
+                        .html("GEOID: " + d.properties.GEOID10);
+                  tooltip_details_1.classed('hidden', false)
+                        .html("Total outgoing trips: " + d.total_trips);
+                }
+      })
+      .on('mouseout', function() {
+                if (second_tooltip) {
+                  tooltip_id_2.classed('hidden', true);
+                  tooltip_details_2.classed('hidden', true);
+                }
+                else {
+                  tooltip_id_1.classed('hidden', true);
+                  tooltip_details_1.classed('hidden', true);
+                }
+      });
 
     function projectPoint(x, y) {
           var point = map.latLngToLayerPoint(new L.LatLng(y, x));
@@ -72,6 +183,7 @@ $(document).ready(function(){
 
     //change position of svg when resize or zoom
     map.on("viewreset", reset);
+    map.on("zoom", reset);
     reset();
 
     function reset() {
@@ -86,14 +198,231 @@ $(document).ready(function(){
           .style("top", topLeft[1] + "px");
 
       g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
       tracts.selectAll('path').attr("d", path);
+      //update position of tracts
       tracts.each(function(d) {
         var position = path.centroid(d);
         d.x = position[0];
         d.y = position[1];
       });
 
+      //redraw the lines
+      tracts.selectAll('line').attr("x1", function(d) { return d.source.x})
+              .attr("x2", function(d) { return d.target.x})
+              .attr("y1", function(d) { return d.source.y})
+              .attr("y2", function(d) { return d.target.y})
+              .attr("stroke","gray")
+              .attr("stroke-width", function(d) {return lineScale(d.weight)});
+
+
     } //end of reset function
+
+    function updateMap() {
+
+          var tractById = d3.map();
+
+          tracts.data(collection.features)
+                .each(function(d) {
+                      tractById.set(d.properties.GEOID10, d);
+                            d.incoming = [];
+                            d.outgoing = [];
+                            d.total_trips = 0;
+                            var position = path.centroid(d);
+                            d.x = position[0];
+                            d.y = position[1];
+                   });
+
+          trips = trips_seattle_modified.filter(function(d) {return ((d.o_tract != d.d_tract) & (mode.indexOf(+d.mode) != -1) & (purpose.indexOf(+d.d_purpose)!= -1)  & (hour.indexOf(+d.time_start_hhmm) != -1))})
+
+
+          var trips_count = d3.nest()
+              .key(function(d) { return d.o_tract; })
+              .key(function(d) { return d.d_tract; })
+              .rollup(function(trips) { return d3.sum(trips, function(d) {return d.count}) })
+              .entries(trips);
+
+          var max_weight = 0;
+
+          trips_count
+              .forEach(function(d) {
+                      var source = tractById.get(d.key);
+                      d.values.forEach(function(d) {
+                        max_weight = Math.max(max_weight, d.value);
+                        var target = tractById.get(d.key);
+                        link = {source: source, target: target, weight: d.value};
+                        source.total_trips += d.value;
+                        source.outgoing.push(link);
+                        target.incoming.push(link);
+                      });
+                    });
+
+
+          var lineScale = d3.scaleLinear()
+            .domain([0, max_weight])
+            .range([0.2, 20]);
+
+          tracts.selectAll("line")
+                      .remove();
+
+          tracts.selectAll("g")
+                    .selectAll("line")
+                    .data(function(d) {return d.outgoing})
+                    .enter().append("line")
+                      .attr("class","tract_link")
+                      .attr("x1", function(d) { return d.source.x})
+                      .attr("x2", function(d) { return d.target.x})
+                      .attr("y1", function(d) { return d.source.y})
+                      .attr("y2", function(d) { return d.target.y})
+                      .attr("stroke","gray")
+                      .attr("stroke-width", function(d) {return lineScale(d.weight)});
+
+      } //end of update map function
+
+    var margin = {top: 0, right: 15, bottom: 30, left: 45};
+    var width = parseInt(d3.select('#barchart').style('width')) - margin.left - margin.right;
+    var height = parseInt(d3.select('#barchart').style('height')) - margin.top - margin.bottom;
+
+    d3.csv('trips_seattle_modified.csv', function(error, data) {
+
+        var hours = ["12 AM","1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"];
+
+        var dataset = Array(24).fill(0);
+
+        //exclude filtered mode and purpose
+        data.forEach(function(d) {
+            if ((mode.indexOf(+d.mode) != -1) & (purpose.indexOf(+d.d_purpose) != -1)) {
+                dataset[+d.time_start_hhmm] += +d.count
+            }
+        });
+
+        var svg_bar = d3.select("#barchart").append("svg")
+                        .attr("id","svg_bar")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom);
+
+        bar = svg_bar.append("g")
+                .attr("class","chart")
+                .attr("transform", 
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        var xScale = d3.scaleBand()
+            .domain(d3.range(dataset.length))
+            .range([0, width]);
+
+        var yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(dataset)]);
+
+         // append the rectangles for the bar chart
+        bar.selectAll(".bar")
+                .data(dataset)
+              .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d,i) { return xScale(i); })
+                .attr("width", xScale.bandwidth())
+                .attr("y", function(d) { return yScale(d); })
+                .attr("height", function(d) { return height - yScale(d); })
+                .on("click", function(d,i) {
+                  if (hour.length == 24) {
+                     if (d3.select(this).classed("highlighted")) {
+                        hour.splice(hour.indexOf(i),1);
+                        d3.select(this).classed("highlighted",false);
+                     }
+                     else {
+                        hour = [i];
+                        d3.select(this).classed("highlighted",true);
+                     }
+                  }
+                  else {
+                    if (d3.select(this).classed("highlighted")) {
+                      hour.splice(hour.indexOf(i),1);
+                      d3.select(this).classed("highlighted",false);
+                      if (hour.length==0) {
+                        hour = Array.apply(0, Array(24)).map(function (x, y) { return y; });
+                      }
+                    }
+                    else {
+                      hour.push(i);
+                      d3.select(this).classed("highlighted",true);
+                    }
+                  }
+                  updateMap();
+                }); //end of on click function
+
+          bar.append("g")
+            .attr("class","yaxis")
+            .call(d3.axisLeft(yScale));
+
+          bar.append("g")
+            .attr("class","xaxis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale));
+
+        function updateBar() {
+
+            dataset = Array(24).fill(0);
+
+            data.forEach(function(d) {
+              if ((mode.indexOf(+d.mode) != -1) & (purpose.indexOf(+d.d_purpose)!= -1)) {
+                   dataset[+d.time_start_hhmm] += +d.count
+              }
+           });
+
+            var xScale = d3.scaleBand()
+            .domain(d3.range(dataset.length))
+            .range([0, width]);
+
+            var yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, d3.max(dataset)]);
+
+            var svg = d3.select("#barchart")
+              .transition();
+
+           bar.selectAll("rect")
+              .data(dataset)
+              .attr("x", function(d, i) {
+                   return xScale(i);
+               })
+              .attr("y", function(d) {
+                   return yScale(d);
+               })
+              .attr("width", xScale.bandwidth())
+              .attr("height", function(d) {
+                return height - yScale(d);
+              });
+
+          bar.select(".yaxis")
+              .call(d3.axisLeft(yScale));
+
+        } //end of updatebar function
+
+        d3.select('#mode_filter')
+          .selectAll('input').on("change", function() {
+            if (this.checked) {
+              mode.push(+this.value)
+            }
+            else {
+              mode.splice(mode.indexOf(+this.value),1)
+            }
+            updateBar();
+            updateMap();
+          })
+
+      d3.select('#purpose_filter')
+        .selectAll('input').on("change", function() {
+          if (this.checked) {
+            purpose.push(+this.value)
+          }
+          else {
+            purpose.splice(purpose.indexOf(+this.value),1)
+          }
+          updateBar();
+          updateMap();
+        })
+
+    }); //end of load csv function
 
   } //end of ready function
 
